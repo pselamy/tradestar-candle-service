e122a6ff2b27495f531d44537d6142a2de38c799
Patrick Selamy <pselamy@gmail.com>
2022-04-08 21:38:04 -0500

    Add `timeRange` to `getCandles()`.

--- src/main/java/com/verlumen/tradestar/candles/CandleService.java
+++ src/main/java/com/verlumen/tradestar/candles/CandleService.java
@@ -1,8 +1,12 @@
 package com.verlumen.tradestar.candles;
 
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Range;
 import com.verlumen.tradestar.protos.candles.Candle;
 
+import java.time.Instant;
+
+
 public interface CandleService {
-    ImmutableSet<Candle> getCandles();
+    ImmutableSet<Candle> getCandles(Range<Instant> timeRange);
 }
