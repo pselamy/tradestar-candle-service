55bdfd5c5e90c33b525102620cf82f0e03f7cb95
Patrick Selamy <pselamy@gmail.com>
2022-04-08 21:53:16 -0500

    Add `instrument` to `getCandles`.

--- src/main/java/com/verlumen/tradestar/candles/CandleService.java
+++ src/main/java/com/verlumen/tradestar/candles/CandleService.java
@@ -3,9 +3,11 @@ package com.verlumen.tradestar.candles;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Range;
 import com.verlumen.tradestar.protos.candles.Candle;
+import com.verlumen.tradestar.protos.instruments.Instrument;
 
 import java.time.Instant;
 
 public interface CandleService {
-    ImmutableSet<Candle> getCandles(Range<Instant> timeRange);
+    ImmutableSet<Candle> getCandles(Instrument instrument,
+                                    Range<Instant> timeRange);
 }
