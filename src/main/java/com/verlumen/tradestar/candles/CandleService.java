3408bcd755ecc0cf7beb1a393a3a4c6ea3685211
Patrick Selamy <pselamy@gmail.com>
2022-04-08 22:25:06 -0500

    Add `ProtobufHttpMessageConverter` bean.

--- src/main/java/com/verlumen/tradestar/candles/CandleService.java
+++ src/main/java/com/verlumen/tradestar/candles/CandleService.java
@@ -2,10 +2,19 @@ package com.verlumen.tradestar.candles;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.protobuf.ProtobufHttpMessageConverter;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class CandleService {
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(CandleService.class, args);
+    }
+
+    @Bean
+    ProtobufHttpMessageConverter protobufHttpMessageConverter() {
+        return new ProtobufHttpMessageConverter();
     }
 }
